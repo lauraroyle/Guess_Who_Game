e8590d5097972099dd2e96f8e071ded525963113
mariamjaludi <mariam.jaludi@gmail.com>
2019-03-25 15:02:58 +0000

    models updated

--- app/models/game.rb
+++ app/models/game.rb
@@ -1,3 +1,5 @@
 class Game < ApplicationRecord
   has_many :game_characters
+  has_many :players, through: :game_characters
+  belongs_to :player
 end
--- app/models/player.rb
+++ app/models/player.rb
@@ -1,4 +1,13 @@
 class Player < ApplicationRecord
   has_many :player_characteristics
   has_many :characteristics, through: :player_characteristics
+
+
+  has_many :game_characters
+  has_many :games, through: :game_characters
+  has_many :games
+
+
+
+
 end
