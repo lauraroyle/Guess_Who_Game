cc62df7d005a5368534ef9b520a63a41b58903cd
Mariam Jaludi <43250910+Mariamjaludi@users.noreply.github.com>
2019-03-25 14:06:08 +0000

    Delete 20190325115311_create_characteristics.rb

--- db/migrate/20190325115311_create_characteristics.rb
+++
@@ -1,9 +0,0 @@
-class CreateCharacteristics < ActiveRecord::Migration[5.1]
-  def change
-    create_table :characteristics do |t|
-      t.string :type
-      t.string :value
-      t.timestamps
-    end
-  end
-end
