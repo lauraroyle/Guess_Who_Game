e1af5a10c460a14a696e58aba7ebf670cf42e76e
mariamjaludi <mariam.jaludi@gmail.com>
2019-03-25 13:58:59 +0000

    player and characteristics controllers started

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,2 +1,4 @@
 class Player < ApplicationRecord
+  has_many :player_characteristics
+  has_many :characteristics, through: :player_characteristics
 end
--- db/migrate/20190325115311_create_characteristics.rb
+++ db/migrate/20190325115311_create_characteristics.rb
@@ -1,8 +1,8 @@
 class CreateCharacteristics < ActiveRecord::Migration[5.1]
   def change
     create_table :characteristics do |t|
-      t.string :name
-
+      t.string :type
+      t.string :value
       t.timestamps
     end
   end
