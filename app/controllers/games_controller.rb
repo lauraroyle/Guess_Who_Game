f08d0797a93b770b3c1b93193cb364ab49683ac9
mariamjaludi <mariam.jaludi@gmail.com>
2019-03-26 20:34:22 +0000

    questions branch

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,15 +1,46 @@
 class GamesController < ApplicationController
+  before_action :set_game, only: %i[show edit update]
 
   def new
-    @game = Game.new
+    # figure out how to find the current player in the game controller
+    # @player = Player.find(params[:id])
+    # @game = Game.new(score: 0, round: 1, player_id: @player)
   end
 
   def create
-    @game = Game.create(params[:id])
+
   end
 
   def show
+    # use method in game model to set up game characters
+    @game_characters = Game.set_up_game_characters
+    # pick one out of the 25 characters to be the guess who character
+    @guess_who = @game_characters.sample
+    # set up associations in GameCharacters table
+    @game_characters.each do |gc|
+      GameCharacter.create(game: @game, player_id: gc)
+    end
+    #put Question.all in an array that we will remove questions from as they are asked.
+    @question_set = Question.all
+    #find first set of Questions
+    @questions_to_display_to_player = @question_set.sample(4)
+    while @game.round != 10
+      # store whatever question the user has chosen "chosen_question"
+      if
+      # remove that question from the question set.
+      @question_set = @question_set.map {|q| q != chosen_question }
+
+    end
+
+  end
+
+  private
+
+  def set_game
     @game = Game.find(params[:id])
   end
 
+  def game_params
+    params.require(:game).permit(:score, :round, :player_id)
+  end
 end
--- app/models/game.rb
+++ app/models/game.rb
@@ -4,16 +4,38 @@ class Game < ApplicationRecord
   belongs_to :player
   # scope :hair_colour, ->(colour) { where(hair_colour: colour) }
 
-  #method that will return all the characters in a game
+  #method that aliases "Players" as "Characters" for easier reference.
   def characters
     players
   end
 
-  def hair_colour(colour)
-    characters.where(hair_colour: colour)
+  # methods to find all the characters in an active game with a given attribute
+  # ex game.glasses("yes") will give you all the characters who wear glasses.
+  def glasses(presence)
+    characters.where(glasses: presence)
   end
 
-  def eye_colour(colour)
-    characters.where(eye_colour: colour)
+  def facial_hair(presence)
+    characters.where(facial_hair: presence)
   end
+
+  def gender(presence)
+    characters.where(gender: presence)
+  end
+
+  def wears_hat(presence)
+    characters.where(wears_hat: presence)
+  end
+
+  def alive(presence)
+    characters.where(alive: presence)
+  end
+
+  # *************************GAME PLAY***********************************
+
+  # this method will choose 25 characters for a 5x5 board. It will then
+  def self.set_up_game_characters
+    Game.all.sample(25)
+  end
+
 end
