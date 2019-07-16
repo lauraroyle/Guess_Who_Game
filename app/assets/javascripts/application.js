8237ebd8cc78e38a5f124aa87c6ca707fa72495b
github username <github email address>
2019-07-16 18:55:33 +0100

    players.js

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,8 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require rails-ujs
 //= require jquery
-//= require jquery3
+//= require rails-ujs
 //= require popper
 //= require bootstrap-sprockets
 //= require_tree .
